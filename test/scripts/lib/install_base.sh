7db453da5b94fb88b6dfc7f568c170a6b1a66a3c
Artem <2urnix@gmail.com>
2017-10-26 15:45:52 +0300

    feat(install_base.sh): update graphicsmagick to 1.3.26

--- test/scripts/lib/install_base.sh
+++ test/scripts/lib/install_base.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential python git graphicsmagick libcairo2-dev libjpeg62-dev libpango1.0-dev libgif-dev g++ libssl-dev wget unzip
+apt-get install -y curl bzip2 build-essential python git libcairo2-dev libjpeg62-dev libpango1.0-dev libgif-dev g++ libssl-dev wget unzip
 wget https://github.com/AGWA/git-crypt/archive/master.zip && unzip master.zip && cd git-crypt-master && make install
+
+# ffmpeg
+apt-get install -y ffmpeg
+
+# graphicsmagick-1.3.26
+apt-get install -y wget dpkg-dev
+wget https://launchpadlibrarian.net/341012792/graphicsmagick_1.3.26-15.dsc
+wget https://launchpad.net/ubuntu/+archive/primary/+files/graphicsmagick_1.3.26.orig.tar.xz
+wget https://launchpad.net/ubuntu/+archive/primary/+files/graphicsmagick_1.3.26-15.debian.tar.xz
+dpkg-source -x graphicsmagick_1.3.26-15.dsc
+cd graphicsmagick-1.3.26/
+./configure
+make
+make install
